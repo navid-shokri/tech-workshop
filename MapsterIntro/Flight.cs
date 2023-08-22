158dd2e2dfad44b4e17416d78088d8beb5ed1191
Navid Shokri <navid.pdp11@gmail.com>
2023-08-22 10:11:42 +0330

    bogus

--- MapsterIntro/Flight.cs
+++ MapsterIntro/Flight.cs
@@ -3,6 +3,7 @@ namespace MapsterIntro;
 
 public class Flight
 {
+    public string FlightUID() => $"{Origin}:{Destination}:{DepartureDate:yyyyMMdd}:{FlightNo}{Airline}";
     public string Origin { get; set; }
     public string Destination { get; set; }
     public DateTime DepartureDate { get; set; }
